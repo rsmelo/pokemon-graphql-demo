a1df0597f787e9b29c267d0dc22c12f9ea7a848d
rsmelo <rodrigosm87@gmail.com>
2016-12-14 16:32:57 -0200

    add schema creation  in Javascript

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
 'user strict';
 const express = require('express');
 const graphqlHttp = require('express-graphql');
-const { graphql, buildSchema } = require('graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLID
+ } = require('graphql');
 
 const PORT = process.env.PORT || 3000;
 const server = express();
 
-const schema = buildSchema(`
-  type Pokemon {
-    id: ID
-    name: String,
-    type: String,
-    attack: Int,
-    defense: Int,
+const pokemonType = new GraphQLObjectType({
+  name: 'Pokemon',
+  description: 'A pocket monster',
+  fields: {
+    id: {
+      type: GraphQLID,
+      description: 'the pokemon number'
+    },
+    name: {
+      type: GraphQLString,
+      description: "the pokemon name"
+    },
+    type: {
+      type: GraphQLString,
+      description: "the pokemon type"
+    },
+    attack: {
+      type: GraphQLInt,
+      description: "the pokemon base attack value"
+    },
+    defense: {
+      type: GraphQLInt,
+      description: "the pokemon base defense value"
+    }
   }
+});
 
-  type Query {
-    pokemon: Pokemon,
-    pokemons: [Pokemon]
+const queryType = new GraphQLObjectType({
+  name: 'QueryType',
+  description: 'The root query type.',
+  fields: {
+    pokemon: {
+      type: pokemonType,
+      resolve: () => new Promise((resolve) =>  {
+        resolve({
+          id: 2,
+          name: 'Ivysaur',
+          type: 'grass',
+          attack: 62,
+          defense: 63
+        });
+      })
+    }
   }
+});
 
-  type Schema {
-    query: Query
-  }
-`);
+const schema = new GraphQLSchema({
+  query: queryType
+});
 
 const pokemons = [
   {
